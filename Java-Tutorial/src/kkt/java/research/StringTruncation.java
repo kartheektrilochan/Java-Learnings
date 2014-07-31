e84e8afe0899ccc63c19c33a64c960a901f0a4a9
trilochan <trilochan@TRILOCHAN>
2014-07-31 10:04:46 +0530

    String Operations Class file

---
+++ Java-Tutorial/src/kkt/java/research/StringTruncation.java
@@ -0,0 +1,15 @@
+package kkt.java.research;
+
+public class StringTruncation {
+
+	public static void main(String[] args) {
+		String name1=" kartheek ";
+		String name2="kartheek";
+		if(name1.equalsIgnoreCase(name2))
+		{
+			System.out.println("equal");
+		}
+		System.out.println("scs"+name1+"dasda");
+	}
+
+}
